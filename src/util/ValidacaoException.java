4a082fad73bbf40c7f81a1920b03be273b75fd22
ALISSON.PATRICIO <ALISSON.PATRICIO@INFO-D515716.SME.FORTALEZA.CE.GOV.BR>
2019-11-06 17:40:48 -0200

    Versao Inicial

--- src/util/ValidacaoException.java
+++ src/util/ValidacaoException.java
@@ -2,6 +2,11 @@ package util;
 
 public class ValidacaoException extends Exception {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ValidacaoException(String message) {
 		super(message);
 	}
